175a549bbad4cee947468f15cbf7bdebbdf09574
Yehudah <you@example.com>
2023-09-28 22:19:24 +0100

    added pages to /dist folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./about.html", "./*.{html,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./about.html", "./works.html", "./*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         raleway: ['Raleway'],
         croissant: ['Croissant One'],
         poppins: ['Poppins']
+      },
+      backgroundImage: {
+        'suzi-prof': "url('/images/suzi-prof.jpg')",
+        'suzi-chill': "url('/images/suzi-chilled.jpg')",
+        'suzi-active': "url('/images/suzi-active.jpg')",
       }
     },
   },
